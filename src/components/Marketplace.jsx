a098603cd438a0a9a9c84e440ca974b6cda72dae
Folugboji <olugbojiobafemi@gmail.com>
2024-10-08 13:01:50 +0100

    PriceTracker Updates

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -4,7 +4,7 @@ import PriceTracker from "./PriceTracker";
 const Marketplace = () => {
   return (
     <div>
-      <h1>Marketplace</h1>
+      <h1 className="font-bold text-2xl text-center py-4">Marketplace</h1>
       <PriceTracker />
       {/* Other marketplace content */}
     </div>
